bc60672fe6c92a15aca711630c83b04ec249ab73
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-11-08 04:56:57 +0100

    resolver hints

--- scripts/travis-tool.sh
+++ scripts/travis-tool.sh
@@ -52,7 +52,7 @@ BootstrapLinux() {
     # Install an R development environment. qpdf is also needed for
     # --as-cran checks:
     #   https://stat.ethz.ch/pipermail/r-help//2012-September/335676.html
-    sudo aptitude install -R --full-resolver \
+    sudo aptitude install -R --full-resolver -o "Aptitude::ProblemResolver::NonDefaultScore=0" -o "Aptitude::ProblemResolver::InstallScore=20" \
         $(GetUbuntuPackageDecl r-base-dev) \
         $(GetUbuntuPackageDecl r-base-core) \
         $(GetUbuntuPackageDecl r-recommended)
